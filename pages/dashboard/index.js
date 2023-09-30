680b466774d481ecb2b0edbcb27dd5f1ca82f10e
fh-rabbi <frabby983@gmail.com>
2023-09-30 11:46:10 +0600

    Increased profile icon size

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -334,8 +334,8 @@ const Home = () => {
                     {/*<Icon icon="mdi:user-circle" color="white" />*/}
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="18"
+                      width="24"
+                      height="24"
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
